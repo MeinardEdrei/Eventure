cff3d081037de210beeff718312906dc710d1b0d
mnrdsnts <meinardsantoss@gmail.com>
2024-11-06 14:13:12 +0800

    added header dropdown

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,15 @@
+'use client';
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [dropDownOpen, setDropDownOpen] = useState(false);
+
+    const toggleDropDown = () => {
+        setDropDownOpen(!dropDownOpen);
+    }
+
   return (
     <div>
       <section className='p-5' style={{ boxShadow: '0px 1px 1px rgba(255, 255, 255, 0.3)' }}>
@@ -13,7 +20,19 @@ const Header = () => {
                 <Link href=''>Calendar</Link>
                 <Link href=''>Notifications</Link>
             </div>
-            <div><Image src='/profile.png' width={30} height={30} alt='logo' /></div>
+            <div className='relative'>
+                <button
+                    onClick={toggleDropDown}>
+                    <Image src='/profile.png' width={30} height={30} alt='logo' />
+                </button>
+            {dropDownOpen && (
+                <div className='absolute right-0 w-48 bg-white z-10 mt-2 rounded-md'>
+                    <Link href='' className='block text-gray-700 text-sm hover:bg-gray-100 px-4 py-2 hover:rounded-md'>Profile</Link>
+                    <Link href='' className='block text-gray-700 text-sm hover:bg-gray-100 px-4 py-2 hover:rounded-md'>Login</Link>
+                </div>
+            )}
+                
+            </div>
         </div>
       </section>
     </div>

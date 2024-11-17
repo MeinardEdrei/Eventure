fdac57537cc9cd04b9e5fccb3b2785a13af50b1e
mnrdsnts <meinardsantoss@gmail.com>
2024-11-17 13:57:54 +0800

    Redesigned dropdown profile

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,12 +2,14 @@
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState, useRef } from 'react'
 
 const Header = () => {
     const [dropDownOpen, setDropDownOpen] = useState(false);
     const dropDownRef = useRef(null);
     const { data: session } = useSession();
+    const router = useRouter();
 
     // console.log("session header: ", session);
 
@@ -62,24 +64,35 @@ const Header = () => {
             
             <div className='relative w-[75%] flex justify-end mr-5'>
                 <button
-                    onClick={toggleDropDown}>
+                    onClick={() => { if(session) {toggleDropDown();} else{router.push('/Login');} }}>
                     <Image src='/profile.png' width={30} height={30} alt='logo' />
                 </button>
 
             {dropDownOpen && (
-                <div ref={dropDownRef} className='absolute right-0 w-48 bg-white z-10 mt-2 rounded-md'>
-                    <Link href='' className='block text-gray-700 text-sm hover:bg-gray-100 px-4 py-2 hover:rounded-md'>Profile</Link>
+                <div ref={dropDownRef} className='absolute right-[2%] w-72 bg-black z-10 mt-2 rounded-md border-white/30 border xl:top-[2vw] lg:top-[5vw] p-2'>
                     {session ? (
                         <>
-                        <button onClick={() => signOut()} className='block text-gray-700 text-sm hover:bg-gray-100 px-4 py-2 hover:rounded-md'>Sign out</button>
+                            <div className='flex justify-center items-center mb-2'>
+                                <div className='flex justify-center w-14'>
+                                    <Image src='/profile.png' width={35} height={35} alt='logo' />
+                                </div>
+                                <div className='flex flex-col overflow-hidden'>
+                                    <span className='text-white text-base'>{session?.user?.username}</span>
+                                    <p className='text-white/30 text-sm overflow-hidden whitespace-nowrap text-ellipsis'>
+                                        {session?.user?.email}
+                                    </p>
+                                </div>
+                                
+                            </div>
+                            <hr className='border-t border-white/30 my-0' />
+                            <Link onClick={() => setDropDownOpen(false)} href='' className='block text-white text-sm hover:bg-gray-100 hover:text-black px-4 py-2 hover:rounded-md mt-2'>Profile</Link>
+                            <button onClick={() => { signOut(); setDropDownOpen(false) }} className='flex text-white text-sm hover:bg-gray-100 hover:text-black px-4 py-2 hover:rounded-md w-full'>Sign out</button>
                         </>
                     ) : (
-                        <Link href='/Login' className='block text-gray-700 text-sm hover:bg-gray-100 px-4 py-2 hover:rounded-md'>Login</Link>
+                        <Link onClick={() => setDropDownOpen(false)} href='/Login' className='block text-white text-sm hover:bg-gray-100 hover:text-black px-4 py-2 hover:rounded-md'>Login</Link>
                     )}
-                    
                 </div>
             )}
-                
             </div>
         </div>
         </div>

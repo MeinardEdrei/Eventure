451af887c047a3e812bc54853a6a62da526d4b8c
mnrdsnts <meinardsantoss@gmail.com>
2024-11-29 00:19:54 +0800

    WIP

--- app/My-Events/page.jsx
+++ app/My-Events/page.jsx
@@ -1,74 +1,178 @@
-'use client';
-import '../css/organizerList.css';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useSession } from 'next-auth/react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+"use client";
+import { useEffect, useState } from "react";
+import "../css/organizerList.css";
+import Link from "next/link";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import PDFUploadModal from "./UploadModal";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 
-function MyEvents () {
-    const { data: session } = useSession();
-    const [events, setEvents] = useState([]);
-    const [loading, setLoading] = useState(false);
+function MyEvents() {
+  const [selected, setSelected] = useState("Pending");
+  const progressOptions = ["Pending", "Approved", "Modified", "Rejected"];
+  const [isOpen, setIsOpen] = useState(false);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+  const { data: session } = useSession();
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                if (session) {
-                    const res = await axios.get(`http://localhost:5000/api/event/organizer-events/${session?.user?.id}`)
-                    setEvents(res.data);
-                }
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setLoading(false);
-            }
-        }
-       fetchData();
-    }, [session]);
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+              setLoading(true);
+              if (session) {
+                  const res = await axios.get(`http://localhost:5000/api/event/organizer-events/${session?.user?.id}`)
+                  setEvents(res.data);
+              }
+          } catch (error) {
+              console.log(error);
+          } finally {
+              setLoading(false);
+          }
+      }
+      fetchData();
+  }, [session]);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  const handleSelect = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
 
-    return (
-        <>
-        <div className="container">
+  const handleFileUpload = (files) => {
+    console.log("Files to upload:", files);
+  };
+
+  useEffect(() => {
+    const filtered = events.filter((event) => event.status === selected);
+    setFilteredEvents(filtered);
+  }, [selected]);
+
+  if (loading) return <div>Loading...</div>;
+  
+
+  return (
+    <div className="org-list-mnc">
+      <div>
+        <div className="flex flex-row items-end justify-between mb-2">
+          <div className="flex flex-col">
             <div className="eventTitle">My Events</div>
-            <div className="eventDescription">A personalized dashboard to view, manage, and track all your upcoming and past events.</div>
+            <div className="eventDescription">
+              A personalized dashboard to view, manage, and track all your
+              upcoming and past events.
+            </div>
+          </div>
 
-            { events.map((event) => (
-                <Link key={event.id} href={`/Event-Details/${event.id}`}>
-                <div className="containerngmgacards">
-                    <div className="organizerEventCard">
-                        <div className="organizerImage">
-                            <img src={`http://localhost:5000/api/event/uploads/${event.eventImage}`} alt="" />
-                        </div>
-                        <div className="cardDetails">
-                            <div className="cardTitle">{event.title}</div>
-                            <div className="cardInfo">
-                                <p>{event.location}</p>
-                                <p>
-                                    {new Date(event.dateStart).toLocaleDateString('en-US', {
-                                        month: 'long',
-                                        day: 'numeric',
-                                        year: 'numeric',
-                                    })} - {new Date(`${event.dateStart.split('T')[0]}T${event.timeStart}`).toLocaleTimeString('en-US', {
-                                        hour: '2-digit',
-                                        minute: '2-digit',
-                                        hour12: true,
-                                    })}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </Link>
-            ))}
+          <div className="dropdown-container">
+            <button
+              className="dropdown-button"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {selected}
+              <i
+                className={`fas fa-chevron-down dropdown-icon ${
+                  isOpen ? "rotate" : ""
+                }`} 
+              ></i>
+            </button>
+
+            {isOpen && (
+              <div className="dropdown-menu">
+                {progressOptions.map((option) => (
+                  <button
+                    key={option}
+                    className={`dropdown-item ${
+                      selected === option ? "selected" : ""
+                    }`}
+                    onClick={() => handleSelect(option)}
+                  >
+                    {option}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-        </>
-    )
+
+        <div className="line-container w-[100%] h-auto">
+          <hr className="border border-solid border-[#ffffff62]" />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center w-[100%]">
+        <div className="content-wrapper flex flex-col items-start gap-4 w-[80%]">
+          <div className="card-status">
+            <h3>{selected}</h3>
+          </div>
+
+          <div className="cards-container w-full">
+            {filteredEvents.length === 0 ? (
+              <div className="text-center w-full text-gray-500">
+                No events found in {selected} status
+              </div>
+            ) : (
+              filteredEvents.map((event) => (
+                <div key={event.id} className="organizerEventCard mb-4">
+                  <div className="organizerImage">
+                    <img src={`http://localhost:5000/api/event/uploads/${event.eventImage}`} alt={event.title} />
+                  </div>
+
+                  <div className="cardDetails w-full">
+                    <div className="flex flex-row justify-between items-start gap-10">
+                      <div className="flex flex-col mb-4">
+                        <div className="cardTitle">{event.title}</div>
+                        <div className="cardInfo">
+                          <p>{event.location}</p>
+                          <p>{new Date(event.dateStart).toLocaleDateString('en-us', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          })}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-row gap-4 justify-end">
+                      {/* Button: See Details */}
+                      <Link href={`/Event-Details/${event.id}`}>
+                        <div className="bg-transparent hover:bg-[#ffffff]/50 transition-all border border-[#ffffff]/25 flex items-center gap-2 px-4 py-2 rounded">
+                          <i
+                            className="fa fa-info-circle text-[0.8rem]"
+                            aria-hidden="true"
+                          ></i>
+                          <button className="text-[0.8rem]">See Details</button>
+                        </div>
+                      </Link>
+
+                      {/* Button: Upload Requirements */}
+                      <div className="bg-[#b6b6b6] hover:bg-[#6a6a6a] text-[#000000] hover:text-white transition-all flex items-center gap-2 px-4 py-2 rounded">
+                        <i
+                          className="fa fa-plus text-[0.8rem]"
+                          aria-hidden="true"
+                        ></i>
+                        <button
+                          className="text-[0.8rem]"
+                          onClick={() => setIsUploadModalOpen(true)}
+                        >
+                          Upload Files
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+
+            <PDFUploadModal
+              isOpen={isUploadModalOpen}
+              onClose={() => setIsUploadModalOpen(false)}
+              onUpload={handleFileUpload}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyEvents;
